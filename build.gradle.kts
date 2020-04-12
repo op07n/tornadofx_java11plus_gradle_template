d987fd9a32cb8380f6f4bad19dc163978ac82d90
xqdd <1032453509@qq.com>
2020-04-12 16:39:05 +0800

    complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,23 @@
 plugins {
     kotlin("jvm") version "1.3.71"
-
     id("application")
-    id("org.openjfx.javafxplugin") version "0.0.8"
+    id("org.openjfx.javafxplugin") version "0.0.9-SNAPSHOT"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-
-
-
-
 repositories {
+    mavenLocal()
     mavenCentral()
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
 }
-
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    implementation("no.tornado:tornadofx:2.0.0-SNAPSHOT") {
+        exclude(group = "org.openjfx")
+    }
 }
-
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "1.8"
@@ -27,7 +25,15 @@ tasks {
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
     }
-    javafx{
-        version
+
+    javafx {
+        version = "14"
+        modules("javafx.controls")
+
+    }
+
+    application {
+        mainClassName = "com.example.demo.app.MyApp"
     }
 }
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,10 @@
 rootProject.name = "hello-tornadofx"
 
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven("https://plugins.gradle.org/m2/")
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+    }
+}
