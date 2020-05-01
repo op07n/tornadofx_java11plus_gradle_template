c8982a3d1d5752bebb9165d5767fa30ecc76c24f
op07n <39182105+op07n@users.noreply.github.com>
2020-05-01 12:03:58 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,24 @@
 rootProject.name = "hello-tornadofx"
 
 pluginManagement {
+    
+  plugins {
+    repositories {
+      gradlePluginPortal()
+    }
+    val kotlinVersion: String by extra
+    val springBootVersion: String by extra
+    val versionsPluginVersion: String by extra
+    val dependencyManagementVersion: String by extra
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("org.springframework.boot") version springBootVersion
+    id("com.github.ben-manes.versions") version versionsPluginVersion
+    id("io.spring.dependency-management") version dependencyManagementVersion
+  }    
+    
+  
+    
     repositories {
         mavenLocal()
         mavenCentral()
@@ -8,3 +26,7 @@ pluginManagement {
         maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
+
+
+val rootProjectName: String by extra
+rootProject.name = rootProjectName
