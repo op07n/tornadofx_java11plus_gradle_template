0cacfb174295a41ef3ebaeb971c8e04ebeb65451
op07n <39182105+op07n@users.noreply.github.com>
2020-05-01 12:15:02 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.3.71"
     id("application")
     id("org.openjfx.javafxplugin") version "0.0.9-SNAPSHOT"
+    id 'org.jetbrains.kotlin.jvm' version '1.3.71' apply(false)
 }
 
 group = "org.example"
@@ -12,7 +13,11 @@ repositories {
     mavenCentral()
     jcenter()
     maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven { url "https://repo.maven.apache.org/maven2" }
+    maven { url 'https://jitpack.io' }
 }
+
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("no.tornado:tornadofx:2.0.0-SNAPSHOT") {
